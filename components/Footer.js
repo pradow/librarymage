198ec07fe612c2218787633597244207f6cff8f3
pradow <pradochris+pradow@protonmail.com>
2022-03-28 01:11:38 -0700

    add footer elements and styles

---
+++ components/Footer.js
@@ -0,0 +1,12 @@
+function Footer() {
+    return (
+        <footer className="main-footer">
+            <div className="footer-links">
+                <a href="https://pradow.com/" className="footer-link">Pradow</a>
+                <a href="https://docs.magicthegathering.io/" className="footer-link">Data Used</a>
+            </div>
+        </footer>
+    )
+}
+ 
+export default Footer;
